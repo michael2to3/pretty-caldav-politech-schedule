e2857676e619724ed987d1ea215153b0bae8717f
Michael <michael2ga3@gmail.com>
2023-02-14 23:38:42 +0300

    Add example of use

--- app/src/main/java/pretty/schedule/App.java
+++ app/src/main/java/pretty/schedule/App.java
@@ -1,6 +1,18 @@
 package pretty.schedule;
 
+import java.time.Instant;
+
+import pretty.schedule.Scraper.Scraper;
+
 public class App {
     public static void main(String[] args) {
+        var url = "https://ruz.spbstu.ru/";
+        var s = new Scraper(url);
+        try {
+            System.out.println(s.getScheduleOfWeek("36267", Instant.now()).getWeek().getDateEnd() + "");
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
     }
 }

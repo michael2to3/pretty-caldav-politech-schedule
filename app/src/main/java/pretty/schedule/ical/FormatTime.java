56746ab94a1e6b5d8ce42d9b65607e4bc0c2d322
Michael <michael2ga3@gmail.com>
2023-02-23 21:04:12 +0300

    Default format time to UTC+0

--- app/src/main/java/pretty/schedule/ical/FormatTime.java
+++ app/src/main/java/pretty/schedule/ical/FormatTime.java
@@ -1,6 +1,11 @@
 package pretty.schedule.ical;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class FormatTime {
@@ -10,9 +15,12 @@ public class FormatTime {
   final int minute;
 
   public FormatTime(final String time) {
-    LocalTime ltime = LocalTime.parse(time, formatter);
-    hour = ltime.getHour();
-    minute = ltime.getMinute();
+    LocalTime localTime = LocalTime.parse(time, formatter);
+    Instant instant = LocalDateTime.of(LocalDate.now(), localTime).atZone(ZoneId.systemDefault()).toInstant();
+    Instant utcInstant = instant.atOffset(ZoneOffset.UTC).toInstant();
+    LocalTime utcTime = LocalDateTime.ofInstant(utcInstant, ZoneOffset.UTC).toLocalTime();
+    hour = utcTime.getHour();
+    minute = utcTime.getMinute();
   }
 
   public int getHour() {

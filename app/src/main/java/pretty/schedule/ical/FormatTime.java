6371660c41fc96181e0c17c8a8906c1d42a46ef1
Michael <michael2ga3@gmail.com>
2023-02-23 15:24:54 +0300

    Fix not correct calc hour and min

--- app/src/main/java/pretty/schedule/ical/FormatTime.java
+++ app/src/main/java/pretty/schedule/ical/FormatTime.java
@@ -6,15 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class FormatTime {
     final static private String FORMAT = "HH:mm";
     final static private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMAT);
-    final static private int BASE = 60;
     final int hour;
     final int minute;
 
     public FormatTime(final String time) {
         LocalTime ltime = LocalTime.parse(time, formatter);
-        int itime = ltime.getMinute();
-        hour = itime % BASE;
-        minute = (itime - itime % BASE) / BASE;
+        hour = ltime.getHour();
+        minute = ltime.getMinute();
     }
 
     public int getHour() {

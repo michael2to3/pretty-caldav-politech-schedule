fd7c6fbd5d3c35bacc4d5c4318d59ceddcaed711
Michael <michael2ga3@gmail.com>
2023-02-23 15:24:12 +0300

    Replace LocalTime to Instant for parse time

--- app/src/main/java/pretty/schedule/ical/FormatSchedule.java
+++ app/src/main/java/pretty/schedule/ical/FormatSchedule.java
@@ -1,51 +1,49 @@
 package pretty.schedule.ical;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 import net.fortuna.ical4j.model.component.VEvent;
 import pretty.schedule.scheme.Lesson;
 import pretty.schedule.scheme.ScheduleOfDay;
 
 public class FormatSchedule {
+	// @FIX duplicate generate timezone
 	final static private String FORMAT_DATE = "yyyy-M-d";
+	final static private String TIME_ZONE = "Erope/Moscow";
+	private static final java.util.TimeZone TZ = TimeZone.getTimeZone(TIME_ZONE);
 	private final ScheduleOfDay schedule;
-	private LocalDate date;
+	private Instant date;
 
 	public FormatSchedule(final ScheduleOfDay schedule) {
 		this.schedule = schedule;
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(FORMAT_DATE);
-		this.date = LocalDate.parse(schedule.getDate(), dateFormatter);
+		var local = LocalDate.parse(schedule.getDate(), dateFormatter);
+		this.date = local.atStartOfDay().toInstant(TZ.toZoneId().getRules().getOffset(Instant.now()));
 	}
 
 	public int getMonth() {
-		return date.getMonth().getValue();
+		return date.atZone(TZ.toZoneId()).getMonth().getValue();
 	}
 
 	public int getDayOfMonth() {
-		return date.getDayOfMonth();
+		return date.atZone(TZ.toZoneId()).getDayOfMonth();
 	}
 
 	public int getYear() {
-		return date.getYear();
+		return date.atZone(TZ.toZoneId()).getYear();
 	}
 
 	public int getHourOfDay() {
-		return date.getYear();
+		return date.atZone(TZ.toZoneId()).getHour();
 	}
 
 	public ScheduleOfDay getSchedule() {
 		return schedule;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
 	}
 
 	public List<VEvent> getEvent() {
@@ -55,8 +53,29 @@ public class FormatSchedule {
 		int day = getDayOfMonth();
 		for (Lesson lesson : schedule.getLessons()) {
 			FormatLesson formatLesson = new FormatLesson(lesson);
-			list.add(formatLesson.generateEvent(year, month, day));
+			VEvent event = formatLesson.generateEvent(year, month, day);
+			list.add(event);
 		}
 		return list;
+	}
+
+	public static String getFormatDate() {
+		return FORMAT_DATE;
+	}
+
+	public static String getTimeZone() {
+		return TIME_ZONE;
+	}
+
+	public static java.util.TimeZone getTz() {
+		return TZ;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
 	}
 }

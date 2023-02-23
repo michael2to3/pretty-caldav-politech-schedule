8c2404bfa03e91b643ec8e8021343a7c2083ddeb
Michael <michael2ga3@gmail.com>
2023-02-23 21:01:13 +0300

    Add logger to Json

--- app/src/main/java/pretty/schedule/util/Json.java
+++ app/src/main/java/pretty/schedule/util/Json.java
@@ -1,13 +1,19 @@
 package pretty.schedule.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Json {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Json.class);
+
   public static String convertString(Object obj) {
     try {
       return new ObjectMapper().writeValueAsString(obj);
     } catch (JsonProcessingException e) {
+      LOGGER.error(e.toString());
       return null;
     }
   }

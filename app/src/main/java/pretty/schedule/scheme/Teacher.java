6e6959fd1c45af2e79feb5a92dbd86330f51d375
Michael <michael2ga3@gmail.com>
2023-02-24 17:03:11 +0300

    Add constructors for Teacher like empty and each fields

--- app/src/main/java/pretty/schedule/scheme/Teacher.java
+++ app/src/main/java/pretty/schedule/scheme/Teacher.java
@@ -1,5 +1,7 @@
 package pretty.schedule.scheme;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /** Represents a teacher with its relavant information. */
@@ -24,6 +26,43 @@ public class Teacher {
   private String grade;
   /** The chait of the teacher. */
   private String chair;
+
+  public Teacher() {
+  }
+
+  public Teacher(int id, int oid, String fullName, String firstName, String middleName, String lastName, String grade,
+      String chair) {
+    this.id = id;
+    this.oid = oid;
+    this.fullName = fullName;
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.grade = grade;
+    this.chair = chair;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (!(o instanceof Teacher))
+      return false;
+    Teacher teacher = (Teacher) o;
+    return id == teacher.id &&
+        oid == teacher.oid &&
+        fullName.equals(teacher.fullName) &&
+        firstName.equals(teacher.firstName) &&
+        middleName.equals(teacher.middleName) &&
+        lastName.equals(teacher.lastName) &&
+        grade.equals(teacher.grade) &&
+        chair.equals(teacher.chair);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, oid, fullName, firstName, middleName, lastName, grade, chair);
+  }
 
   public int getId() {
     return id;
